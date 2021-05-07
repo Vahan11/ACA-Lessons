f64c1ce8390f0f47f3891a70030abebe1c9cc041
Vahan Sargsyan <vahansargsyan1990@gmail.com>
2021-05-07 13:27:22 +0400

    fix: check if value is Infinity (type: number)

--- lesson-05-03-05-2021/main-tasks/parseint-impl.js
+++ lesson-05-03-05-2021/main-tasks/parseint-impl.js
@@ -1,32 +1,34 @@
-function myParseInt(str) {
-  let result = "";
-
-  str = str.trim();
-
-  if (str.charAt(0) === "+") {
-    str = str.slice(1);
-  }
-
-  if (
-    (str.charAt(0) === "0" && str.charAt(1) === "x") ||
-    (str.charAt(0) === "0" && str.charAt(1) === "X")
-  ) {
-      return "This part of implementation are missing.";
-    // changeBaseAndParse();
-  }
-
-  for (let i = 0; i <= str.length; i++) {
-    if (isNaN(str[i])) {
-      if (result.length) {
-        return Number(result);
-      } else {
-        return NaN;
-      }
-    } else {
-      result += str[i];
+function myParseInt(str){
+    let result = "";
+  
+    if(str === Infinity){
+      return NaN;
     }
+  
+    str = str.trim();
+  
+    if(str.charAt(0) === "+"){
+      str = str.slice(1);
+    }
+  
+    if ((str.charAt(0) === "0" && str.charAt(1) === "x") || 
+      (str.charAt(0) === "0" && str.charAt(1) === "X"))
+    {
+      // changeBaseAndParse();
+    }
+  
+    for (let i = 0; i <= str.length; i++) {
+      if(isNaN(str[i])){
+        if(result.length){
+          return Number(result);
+        }else{
+          return NaN;
+        }
+      }else{
+        result += str[i];
+      }
+    }
+  
+    return result;
   }
-
-  return result;
-}
-console.log(myParseInt("015"));
+  console.log(myParseInt(+"Infinity"));
